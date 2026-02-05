854f2b43d821e0d1d72261291b7db7eb4c9a25e2
AneeshAhmed2001 <106055696+AneeshAhmed2001@users.noreply.github.com>
2026-02-05 17:37:01 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const path = require("path");
 
 const app = express();
 
-// Serve frontend files from public folder
 app.use(express.static("public"));
 
-// API endpoint to send mock dashboard data
 app.get("/api/dashboard", (req, res) => {
   res.sendFile(path.join(__dirname, "data.json"));
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("✅ Server running at http://localhost:3000");
+  console.log("Server running at http://localhost:3000");
 });
